abbb79404022096e0b0c5809cef23b5346fa5a74
derushio <derushio@gmail.com>
2024-11-17 22:13:33 +0900

    seed test user & update manual

---
+++ src/tools/seedTestUser.ts
@@ -0,0 +1,34 @@
+import { upperzero } from '@/data-access/types/zod/utils';
+import { PrismaClient } from '@prisma/client';
+import bcrypt from 'bcrypt';
+import { z } from 'zod';
+
+const prisma = new PrismaClient();
+
+const user = {
+  name: 'テストユーザー',
+  email: 'test@example.com',
+  password: 'password',
+};
+
+async function main() {
+  console.log(user);
+  const hash = await bcrypt.hash(
+    user.password,
+    upperzero(z.number()).parse(Number(process.env.SALT_ROUNDS)),
+  );
+
+  await prisma.user.create({
+    data: {
+      name: user.name,
+      UserSecret: {
+        create: {
+          email: user.email,
+          passwordHash: hash,
+        },
+      },
+    },
+  });
+}
+
+void main();
