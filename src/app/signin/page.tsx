7c93bf19bc313aedee73710cfc21d67ae793842f
derushio <derushio@gmail.com>
2024-11-17 22:01:07 +0900

    nuxt-example

---
+++ src/app/signin/page.tsx
@@ -0,0 +1,9 @@
+import { SigninForm } from '@/components/auth/signin/SigninForm';
+
+export default async function SigninPage() {
+  return (
+    <div className='flex w-full h-full justify-center items-center'>
+      <SigninForm />
+    </div>
+  );
+}
