5a951f8b2212d1d66261a73d5bfa45509b77cfbb
derushio <derushio@gmail.com>
2024-11-28 01:58:06 +0900

    design

--- src/app/posts/PostCard.tsx
+++ src/app/posts/PostCard.tsx
@@ -8,11 +8,11 @@ import { Card } from 'flowbite-react';
 export async function PostCard({ post }: { post: Post }) {
   return (
     <Card>
+      <div>{post.title}</div>
+      <hr className='h-px bg-gray-200 border-0 dark:bg-gray-700' />
       <div className='text-right'>
         {format(post.createdAt, ...japaneseDateTimeLocaleFormats)}
       </div>
-      <div>{post.title}</div>
-      <hr className='h-px bg-gray-200 border-0 dark:bg-gray-700' />
       <div className='min-h-64'>{post.body}</div>
     </Card>
   );
